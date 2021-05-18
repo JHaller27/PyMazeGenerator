b5cc6026eea8df01e37eb7e5c9abc4ecd7fc42f5
james <jameshaller27@gmail.com>
2021-05-18 02:38:15 -0700

    Clean-up main + add set custom seed

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
+import random
+
 from sidewinder import carve_maze
 from maze import Maze, print_maze
 
 
-def main(size: int):
+def print_hex(val):
+    return f'{val:02X}'
+
+
+def main(size: int, seed: int = None):
+    if seed is None:
+        seed = random.randrange(100000)
+    random.seed(seed)
+
+    print(f'{seed=}')
+
     maze = Maze(size, size, lambda r, c: r * 0x10 + c)
-    print_maze(maze, lambda v: f'{v:02X}')
+
+    print_maze(maze, print_hex)
     print()
+
     carve_maze(maze)
-    print_maze(maze, lambda v: f'{v:02X}')
+
+    print_maze(maze, print_hex)
 
 
 if __name__ == "__main__":
