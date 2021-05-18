798a2f3ebd970a746f73ae347c8dafc6aa968104
james <jameshaller27@gmail.com>
2021-05-18 02:42:55 -0700

    Add comments

--- main.py
+++ main.py
@@ -4,24 +4,23 @@ from sidewinder import carve_maze
 from maze import Maze, print_maze
 
 
+# Pretty-print maze-cell value as 2-digit hex
 def print_hex(val):
     return f'{val:02X}'
 
 
 def main(size: int, seed: int = None):
+    # Set+print random seed
     if seed is None:
         seed = random.randrange(100000)
     random.seed(seed)
-
     print(f'{seed=}')
 
+    # Create Maze object w/ custom cell values
     maze = Maze(size, size, lambda r, c: r * 0x10 + c)
 
-    print_maze(maze, print_hex)
-    print()
-
+    # Carve paths through the maze, and pretty-print result
     carve_maze(maze)
-
     print_maze(maze, print_hex)
 
 
